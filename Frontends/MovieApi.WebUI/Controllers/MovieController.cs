1e2b1c448907b2f04160e4a6d2a80203ed457d71
burak_cokkececi <98846167+MRCERYANCI@users.noreply.github.com>
2025-04-12 01:14:02 +0300

    ...

---
+++ Frontends/MovieApi.WebUI/Controllers/MovieController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.Controllers
+{
+    public class MovieController : Controller
+    {
+        public async Task<IActionResult> MovieList()
+        {
+            TempData["v1"] = "Film Listesi";
+            TempData["v2"] = "Tüm Filmler";
+            return View();
+        }
+    }
+}
---
+++ Frontends/MovieApi.WebUI/ViewComponents/UILayoutViewComponents/_UILayoutWebUIHeroComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _UILayoutWebUIHeroComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
