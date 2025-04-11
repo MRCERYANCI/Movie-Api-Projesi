d0f42d7b6f1eb26bb12a1b0625b798f67888af24
burak_cokkececi <98846167+MRCERYANCI@users.noreply.github.com>
2025-04-12 02:04:09 +0300

    ...

---
+++ Frontends/MovieApi.WebUI/ViewComponents/UILayoutViewComponents/_UILayoutWebUIFooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _UILayoutWebUIFooterComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/MovieApi.WebUI/ViewComponents/UILayoutViewComponents/_UILayoutWebUIScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieApi.WebUI.ViewComponents.UILayoutViewComponents
+{
+    public class _UILayoutWebUIScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
